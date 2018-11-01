d692ec8b4903d19ca60ddf958a1359c8e5e4b9c6
Lexi Namer <lexinamer@gmail.com>
2018-11-01 19:14:22 -0400

    Update script.js

--- Class18-AJAX_JSON/inclassex/script.js
+++ Class18-AJAX_JSON/inclassex/script.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
     url: 'https://www.govtrack.us/api/v2/role?current=true&role_type=senator',
     success: function(data){
       // Console log the data and examine it
+      console.log(data);
+      
+      // Pass the data to a function
       doWork(data);
     },
     error: function(error) {
@@ -13,18 +16,13 @@ $(document).ready(function(){
     }
   })
 
+   // Write a function to do all the work
   function doWork(data){
+    
+    // Figure out what data you need and use it
     for(var i=0; i<data.objects.length; i++) {
       console.log(data.objects[i].person.firstname);
       console.log(data.objects[i].person.birthday);
     }
   }
-
-
-  // Pass the data to a function
-
-  // Write a function to do all the work
-
-  // Figure out what data you need and use it
-
 });
